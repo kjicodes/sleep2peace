3cebdb8c85d561cc7c53dea06696dc7b50c4b85f
KI <kateleenissa@theMother.local>
2022-09-05 22:12:54 -0400

    Update

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
-require('./config/database');
 require('dotenv').config()
+require('./config/database');
+
 
 var indexRouter = require('./routes/index');
 var sleepsRouter = require('./routes/sleeps');
